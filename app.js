3352ab440cca9f71b22853145822aded1c65d256
rejoyan <rejoyanislam22@gamil.com>
2022-05-01 15:58:25 +0600

    Data-Structure(resut)

---
+++ app.js
@@ -0,0 +1,690 @@
+const students=[
+   {
+      studentName: "joy",
+      roll : 1,
+      age : 8,
+      class : "five",
+      gender : "male",
+      location : "bogura",
+      mark:{
+         Bangla : 80,
+         English : 88,
+         Math : 93,
+         SocialWork : 87,
+         Religion : 84,
+         Ict : 87 
+
+      }
+   },
+   {
+      studentName: "juli",
+      roll : 2,
+      age : 9,
+      class : "five",
+      gender : "female",
+      location : "sylhet",      
+      mark:{
+         Bangla : 83,
+         English : 83,
+         Math : 63,
+         SocialWork : 87,
+         Religion : 74,
+         Ict : 93
+
+      }
+   },
+   {
+      studentName: "alo",
+      roll : 3,
+      age : 7,
+      class : "five",
+      gender : "female",
+      location : "dhaka",
+      mark:{
+         Bangla : 83,
+         English : 78,
+         Math : 98,
+         SocialWork : 63,
+         Religion : 81,
+         Ict : 69 
+
+      }
+   },
+   {
+      studentName: "basir",
+      roll : 4,
+      age : 7,
+      class : "five",
+      gender : "male",
+      location : "uttora",
+      mark:{
+         Bangla : 64,
+         English : 73,
+         Math : 80,
+         SocialWork : 89,
+         Religion : 83,
+         Ict : 77 
+
+      }
+   },
+   {
+      studentName: "jamela",
+      roll : 5,
+      age : 10,
+      class : "five",
+      gender : "female",
+      location : "bogura",
+      mark:{
+         Bangla : 83,
+         English : 81,
+         Math : 89,
+         SocialWork : 77,
+         Religion : 74,
+         Ict : 77 
+
+      }
+   },
+   {
+      studentName: "rifa",
+      roll : 6,
+      age : 10,
+      class : "five",
+      gender : "female",
+      location : "bogura",
+      mark:{
+         Bangla : 88,
+         English : 78,
+         Math : 91,
+         SocialWork : 87,
+         Religion : 74,
+         Ict : 92 
+
+      }
+   },
+   {
+      studentName: "shuvo",
+      roll : 7,
+      age : 9,
+      class : "five",
+      gender : "male",
+      location : "pabna",
+      mark:{
+         Bangla : 67,
+         English : 88,
+         Math : 81,
+         SocialWork : 87,
+         Religion : 84,
+         Ict : 87 
+
+      }
+   },
+   {
+      studentName: "sweki",
+      roll : 8,
+      age : 8,
+      class : "five",
+      gender : "female",
+      location : "pabna",
+      mark:{
+         Bangla : 68,
+         English : 80,
+         Math : 91,
+         SocialWork : 87,
+         Religion : 78,
+         Ict : 80 
+
+      }
+   },
+   {
+      studentName: "rahim",
+      roll : 9,
+      age : 11,
+      class : "five",
+      gender : "male",
+      location : "dhaka",
+      mark:{
+         Bangla : 78,
+         English : 68,
+         Math : 89,
+         SocialWork : 27,
+         Religion : 84,
+         Ict : 87 
+
+      }
+   },
+   {
+      studentName: "mumin",
+      roll : 10,
+      age : 9,
+      class : "five",
+      gender : "male",
+      location : "uttora",
+      mark:{
+         Bangla : 78,
+         English : 58,
+         Math : 73,
+         SocialWork : 87,
+         Religion : 84,
+         Ict : 32 
+
+      }
+   },
+   {
+      studentName: "shohagi",
+      roll : 11,
+      age : 9,
+      class : "five",
+      gender : "female",
+      location : "uttora",
+      mark:{
+         Bangla : 78,
+         English : 68,
+         Math : 93,
+         SocialWork : 87,
+         Religion : 74,
+         Ict : 89 
+
+      }
+   },
+   {
+      studentName: "tunir",
+      roll : 12,
+      age : 7,
+      class : "five",
+      gender : "male",
+      location : "rangpur",
+      mark:{
+         Bangla : 48,
+         English : 68,
+         Math : 56,
+         SocialWork : 87,
+         Religion : 84,
+         Ict : 67 
+
+      }
+   },
+   {
+      studentName: "akash",
+      roll : 13,
+      age : 10,
+      class : "five",
+      gender : "male",
+      location : "rangpur",
+      mark:{
+         Bangla : 80,
+         English : 58,
+         Math : 43,
+         SocialWork : 80,
+         Religion : 64,
+         Ict : 67 
+
+      }
+   },
+   {
+      studentName: "somrat",
+      roll : 14,
+      age : 9,
+      class : "five",
+      gender : "male",
+      location : "uttora",
+      mark:{
+         Bangla : 45,
+         English : 48,
+         Math : 93,
+         SocialWork : 87,
+         Religion : 74,
+         Ict : 81
+      }
+   },
+   {
+      studentName: "mina",
+      roll : 15,
+      age : 11,
+      class : "five",
+      gender : "female",
+      location : "rangpur",
+      mark:{
+         Bangla : 71,
+         English : 48,
+         Math : 17,
+         SocialWork : 89,
+         Religion : 84,
+         Ict : 67 
+
+      }
+   },
+   {
+      studentName: "jamil",
+      roll : 16,
+      age :9,
+      class : "five",
+      gender : "male",
+      location : "dhaka",
+      mark:{
+         Bangla : 58,
+         English : 48,
+         Math : 23,
+         SocialWork : 87,
+         Religion : 64,
+         Ict : 47 
+
+      }
+   },
+   {
+      studentName: "polok",
+      roll : 17,
+      age : 7,
+      class : "five",
+      gender : "male",
+      location : "rajshahi",
+      mark:{
+         Bangla : 38,
+         English : 48,
+         Math : 63,
+         SocialWork : 47,
+         Religion : 74,
+         Ict : 77 
+
+      }
+   },
+   {
+      studentName: "isha",
+      roll : 18,
+      age : 8,
+      class : "five",
+      gender : "female",
+      location : "rajshahi",      mark:{
+         Bangla : 72,
+         English : 88,
+         Math : 63,
+         SocialWork : 87,
+         Religion : 54,
+         Ict : 47 
+
+      }
+   },
+   {
+      studentName: "arnob",
+      roll : 19,
+      age : 7,
+      class : "five",
+      gender : "male",
+      location : "rajshahi",
+      mark:{
+         Bangla : 58,
+         English : 78,
+         Math : 23,
+         SocialWork : 57,
+         Religion : 34,
+         Ict : 27 
+
+      }
+   },
+   {
+      studentName: "ohee",
+      roll : 20,
+      age : 6,
+      class : "five",
+      gender : "female",
+      location : "uttora",
+      mark:{
+         Bangla : 88,
+         English : 68,
+         Math : 43,
+         SocialWork : 67,
+         Religion : 44,
+         Ict : 36 
+
+      }
+   },
+   {
+      studentName: "zami",
+      roll :21,
+      age : 7,
+      class : "five",
+      gender : "male",
+      location : "dhaka",
+      mark:{
+         Bangla : 48,
+         English : 48,
+         Math : 53,
+         SocialWork : 81,
+         Religion : 60,
+         Ict : 43 
+
+      }
+   },
+   {
+      studentName: "rumi",
+      roll :22,
+      age : 7,
+      class : "five",
+      gender : "female",
+      location : "rajshahi",
+      mark:{
+         Bangla : 78,
+         English : 44,
+         Math : 93,
+         SocialWork : 67,
+         Religion : 54,
+         Ict : 27 
+
+      }
+   },
+   {
+      studentName: "shadhin",
+      roll :23,
+      age : 10,
+      class : "five",
+      gender : "male",
+      location : "pabna",
+      mark:{
+         Bangla : 78,
+         English : 38,
+         Math : 43,
+         SocialWork : 27,
+         Religion : 64,
+         Ict : 87 
+
+      }
+   },
+   {
+      studentName: "kahar",
+      roll :24,
+      age : 9,
+      class : "five",
+      gender : "male",
+      location : "bogura",
+      mark:{
+         Bangla : 78,
+         English : 68,
+         Math : 63,
+         SocialWork : 17,
+         Religion : 14,
+         Ict : 17 
+
+      }
+   },
+   {
+      studentName: "arman",
+      roll :25,
+      age : 11,
+      class : "five",
+      gender : "male",
+      location : "rajshahi",
+      mark:{
+         Bangla : 48,
+         English : 58,
+         Math : 83,
+         SocialWork : 87,
+         Religion : 84,
+         Ict : 77 
+
+      }
+   },
+   {
+      studentName: "deen",
+      roll :26,
+      age : 11,
+      class : "five",
+      gender : "male",
+      location : "dhaka",
+      mark:{
+         Bangla : 79,
+         English : 81,
+         Math : 63,
+         SocialWork : 47,
+         Religion : 14,
+         Ict : 17 
+
+      }
+   },
+   {
+      studentName: "nainai",
+      roll :27,
+      age : 8,
+      class : "five",
+      gender : "male",
+      location : "rangpur",
+      mark:{
+         Bangla : 42,
+         English : 38,
+         Math : 33,
+         SocialWork : 47,
+         Religion : 54,
+         Ict : 77 
+
+      }
+   },
+   {
+      studentName: "munna",
+      roll :28,
+      age : 11,
+      class : "five",
+      gender : "male",
+      location : "uttora",
+      mark:{
+         Bangla : 22,
+         English : 38,
+         Math : 38,
+         SocialWork : 37,
+         Religion : 74,
+         Ict : 67 
+
+      }
+   },
+   {
+      studentName: "mohini",
+      roll :29,
+      age : 10,
+      class : "five",
+      gender : "female",
+      location : "pabna",
+      mark:{
+         Bangla : 12,
+         English : 18,
+         Math : 33,
+         SocialWork : 47,
+         Religion : 54,
+         Ict : 76
+
+      }
+   },
+   {
+      studentName: "salam",
+      roll :30,
+      age : 9,
+      class : "five",
+      gender : "male",
+      location : "bogura",
+      mark:{
+         Bangla : 38,
+         English : 33,
+         Math : 78,
+         SocialWork : 47,
+         Religion : 74,
+         Ict : 49 
+
+      }
+   }
+]
+
+// Subject & mark
+let markArray=students.map((data,index) => {
+   result= data.mark;
+   return result;
+    
+});
+//Grade Array
+let gradeArray=[];
+let gpaArray=[];
+let cgpaArray=[];
+let totalGrade=[];
+ markArray.forEach(data => {
+     let Bangla=data.Bangla;
+     let English=data.English;
+     let Math=data.Math;
+     let SocialWork=data.SocialWork;
+     let Religion=data.Religion;
+     let Ict=data.Ict;
+     let subject=[Bangla,English,Math,SocialWork,Religion,Ict]
+     let result;
+     let aGrade=[];
+
+
+
+// Create Grade Array
+
+     for ( i=0;i<=5;i++){
+          if(subject[i] < 33){
+            result= "F"
+          }
+          else if(subject[i] >= 33 && subject[i] < 40){
+            result= "D"
+          }
+          else if(subject[i] >= 40 && subject[i] <50 ){
+            result= "C"
+          }
+          else if(subject[i] >= 50 && subject[i] < 60){
+             result= "B"
+          }
+          else if(subject[i]>=60 && subject[i]<70){
+            result= "A-"
+          }
+          else if(subject[i]>=70 && subject[i]<80){
+            result= "A"
+          }
+          else if(subject[i]>=80 && subject[i]<=100){
+             result= "A+"
+          }
+      aGrade.push(result)
+     }
+      gradeArray.push(aGrade)
+
+
+// Create  gpa  Araay  
+
+
+      let aGpa=[];
+   for(i=0;i<=5;i++){
+      if(aGrade[i] == "A+" ){
+            gpa= 5
+        }
+      else if( aGrade[i] == "A"){
+         gpa = 4
+         }
+      else if(aGrade[i] == "A-"){
+         gpa = 3.5
+         }
+      else if(aGrade[i] == 'B'){
+         gpa = 3
+      }
+      else if(aGrade[i]== 'C'){
+         gpa = 2
+         }
+      else if(aGrade[i]=='D'){
+         gpa = 1
+         }
+      else if(aGrade[i]=='F'){
+            gpa = 0
+      }
+         aGpa.push(gpa)
+   }
+
+   gpaArray.push(aGpa)
+
+
+
+
+// Create Cgpa Array 
+
+
+   let sum=0;
+   let cgpa;
+   if(aGpa.includes(0)){
+      cgpa =0 ;
+      }
+   else{
+         aGpa.forEach((data,index)=>{
+         sum += +data
+         cgpa =(sum / 6).toFixed(2);
+                        })
+      }
+
+   cgpaArray.push(cgpa)
+
+
+ })  
+
+ //final Grade 
+
+      let tGrade;
+    for(i=0;i<cgpaArray.length;i++){
+       if(cgpaArray[i]<1){
+          tGrade= "F"
+       }
+       else if(cgpaArray[i] >=1 && cgpaArray[i]<2){
+          tGrade= "D"
+       }
+      else if(cgpaArray[i] >= 2 && cgpaArray[i]<3){
+         tGrade= "C"
+      }
+      else if(cgpaArray[i] >= 3 && cgpaArray[i]<3.5){
+         tGrade= "B"
+      }
+      else if(cgpaArray[i] >= 3.5 && cgpaArray[i]<4){
+         tGrade= "A-"
+      }
+      else if(cgpaArray[i] >= 4 && cgpaArray[i]<5){
+         tGrade= "A"
+      }
+      else if(cgpaArray[i] >= 5 ){
+         tGrade= "A+"
+      }
+      totalGrade.push(tGrade)
+    }
+
+
+
+ //final Result 
+
+
+
+
+   for(i=0;i<30;i++){
+     console.log(`
+
+
+     name    :       ${students[i].studentName}
+     Roll    :       ${students[i].roll}
+     Class   :       ${students[i].class}
+     Gender  :       ${students[i].gender}
+     result  :  
+                                
+        Subject              mark        Grade    gpa
+        -------              ----        -----    ---
+        1.Bangla      :       ${students[i].mark.Bangla}          ${gradeArray[i][0]  }       ${gpaArray[i][0]}
+        2.English     :       ${students[i].mark.English}          ${gradeArray[i][1]  }       ${gpaArray[i][1]}
+        3.Math        :       ${students[i].mark.Math}          ${gradeArray[i][2]  }       ${gpaArray[i][2]}
+        4.SocialWork  :       ${students[i].mark.SocialWork}          ${gradeArray[i][3]  }       ${gpaArray[i][3]}
+        5.Religion    :       ${students[i].mark.Religion}          ${gradeArray[i][4]  }       ${gpaArray[i][4]}
+        6.Ict         :       ${students[i].mark.Ict}          ${gradeArray[i][5]  }       ${gpaArray[i][5]}
+----------------------------------------------------------------------------------       
+         Total                            ${totalGrade[i]}      ${cgpaArray[i]}
+     `) 
+   
+}
+
+
+
+
+
+
+
+
+
+
+
+
+       
+
